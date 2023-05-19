d3eae9cb851f9933a00e02398ac6d97e06078cf2
AhmedElkhashn <ahmad.alaa6778@gmail.com>
2023-05-19 16:21:47 +0300

    Updated utility class

--- Phase 2/HRSystemApp/src/HRSystemApp/Utility.java
+++ Phase 2/HRSystemApp/src/HRSystemApp/Utility.java
@@ -1,5 +1,26 @@
 package HRSystemApp;
 
-public class Utility {
-	int i;
+import java.sql.*;
+
+public class Utility 
+{
+	public String dburl="jdbc:oracle:thin:@***REMOVED***:1521/***REMOVED***";
+	public String user="test";
+	public String pass="test";
+	Statement stmt;
+	Connection conn;
+	ResultSet rs;
+	
+	public Utility() throws SQLException 
+	{
+		conn=DriverManager.getConnection(dburl, user, pass);
+		stmt=conn.createStatement();
+	}
+	
+	public void terminate() throws SQLException
+	{
+		stmt.close();
+		conn.close();
+	}
+	
 }
